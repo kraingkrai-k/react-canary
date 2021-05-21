76fba294bfcafb5e5b695fb3f216a81564a65195
kraingkrai-k <kraingkrai.k96@gmail.com>
2021-05-21 22:37:53 +0700

    refactor

--- src/pages/authen/component/ModalLogin.tsx
+++ src/pages/authen/component/ModalLogin.tsx
@@ -1,30 +1,86 @@
-import React, {useState, createRef} from 'react';
-import {FormInstance, Modal} from 'antd';
+import React, {useState} from 'react';
+import {Form, Input, Modal} from 'antd';
+import {useDispatch} from "react-redux";
 
-import FormLogin from "./FormLogin";
+import useAxios, {UseService} from "hooks/useAxios";
+import {setAuthenticate} from "store/app";
+import {ACCESS_TOKEN} from "core/utils/storage";
+
+import {ErrorMsg, SuccessMsg} from "../../../component/common/ToastMessage";
+
+import {IFromLogin, IFromLoginField} from "../model/formLogin";
+import AuthService, {IServiceAuth} from "../service/authen-service";
+import {AuthenticateInputMock} from "../mock/authen";
 
 const ModalLogin: React.FunctionComponent = (): React.ReactElement => {
-    const ref = createRef<FormInstance>()
-    const [isModalVisible, setIsModalVisible] = useState<boolean>(true);
+    const {service} = useAxios<IServiceAuth>((axiosInstance) => AuthService(axiosInstance))
+    const [form] = Form.useForm()
 
-    const handleOk = () => {
-        ref.current?.submit()
+    const dispatch = useDispatch();
+
+    const {signInWithUserNameAndPassword} = service()
+
+    const [isModalVisible, setIsModalVisible] = useState<boolean>(true);
+    const [isConfirmLoading, setIsConfirmLoading] = useState<boolean>(false);
+
+    const handlerModal = (isOk: boolean): void => {
+        if (isOk) {
+            setIsConfirmLoading(true)
+            form.submit()
+            return
+        }
         setIsModalVisible(false);
     };
 
-    const handleCancel = () => {
-        setIsModalVisible(false);
+    const onFinish = async (values: IFromLogin) => {
+        try {
+            const {result, err} = await UseService(() => signInWithUserNameAndPassword(values))
+            if (err) {
+                ErrorMsg()
+                return
+            }
+            window.sessionStorage.setItem(ACCESS_TOKEN, result.accessToken)
+            dispatch(setAuthenticate(result))
+            SuccessMsg('Login Success.')
+        } catch (err) {
+            ErrorMsg()
+        } finally {
+            setIsModalVisible(false);
+        }
     };
 
     return (
         <Modal
             title="Login"
             visible={isModalVisible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-            confirmLoading={!isModalVisible}
+            onOk={() => handlerModal(true)}
+            onCancel={() => handlerModal(false)}
+            maskClosable={false}
+            confirmLoading={isConfirmLoading}
         >
-            <FormLogin ref={ref}/>
+            <Form
+                form={form}
+                initialValues={AuthenticateInputMock}
+                onFinish={onFinish}
+                wrapperCol={{span: 24, offset: 1}}
+                labelCol={{span: 4}}
+            >
+                <Form.Item
+                    label="Email"
+                    name={IFromLoginField.email}
+                    rules={[{required: true, message: 'Please input your email!'}]}
+                >
+                    <Input maxLength={12}/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name={IFromLoginField.password}
+                    rules={[{required: true, message: 'Please input your password!'}]}
+                >
+                    <Input.Password maxLength={12}/>
+                </Form.Item>
+            </Form>
         </Modal>
     );
 };
--- src/pages/todo/service/todo-service.ts
+++ src/pages/todo/service/todo-service.ts
@@ -2,10 +2,9 @@ import {AxiosInstance} from "axios"
 
 import {IS_MOCKUP} from "core/utils/env";
 import {UserListMock} from "../mock/userList";
-import {IGetTodoFilter, ITodo, IUser} from "../model/todo";
+import {IUser} from "../model/todo";
 
 export interface ITodoService {
-    getTodoByID: (filter: IGetTodoFilter) => Promise<ITodo>
     getUserList: () => Promise<IUser[]>
 }
 
@@ -15,29 +14,16 @@ const Services = (axiosInstance: AxiosInstance): ITodoService => {
             if (IS_MOCKUP) {
                 return Promise.resolve(UserListMock)
             }
-            try {
-                const {status, data} = await axiosInstance.get(`/users`);
-                if (status !== 200) {
-                    return []
-                }
-                return data
-            } catch (err) {
-                return err
-            }
-        },
-        getTodoByID: async (filter: IGetTodoFilter): Promise<ITodo> => {
-            if (IS_MOCKUP) {
-                return {} as ITodo
-            }
-            try {
-                const {status, data} = await axiosInstance.get(`/todos/${filter.id}`);
-                if (status !== 200) {
-                    return {} as ITodo
-                }
-                return data
-            } catch (err) {
-                throw new Error(err)
-            }
+            return Promise.resolve(UserListMock)
+            // try {
+            //     const {status, data} = await axiosInstance.get(`/users`);
+            //     if (status !== 200) {
+            //         return []
+            //     }
+            //     return data
+            // } catch (err) {
+            //     return err
+            // }
         }
     }
 }
