fcc184bcbc909c948fc01027d122ef24aa7b583e
kraingkrai-k <kraingkrai.k96@gmail.com>
2021-05-22 16:23:44 +0700

    fix test mockup

--- src/pages/todo/service/todo-service.test.tsx
+++ src/pages/todo/service/todo-service.test.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable */
 import {AxiosInstance} from "axios";
 
+import {IS_MOCKUP} from 'core/utils/env'
+
 import TodoService from "./todo-service";
+import {UserListMock} from "../mock/userList";
 
 const get = jest.fn()
 const axiosInstance: AxiosInstance = {
@@ -11,12 +15,18 @@ describe('todoService', () => {
     const service = TodoService(axiosInstance)
 
     it('get user List should be success', async () => {
+
         get.mockResolvedValue([])
 
         const actual = await service.getUserList()
-        expect(actual).toEqual([])
 
-        expect(get).toHaveBeenCalledWith('/users')
-        expect(get).toHaveBeenCalledTimes(1)
+        if (IS_MOCKUP) {
+            expect(actual).toEqual(UserListMock)
+        } else {
+            expect(actual).toEqual([])
+            expect(get).toHaveBeenCalledWith('/users')
+            expect(get).toHaveBeenCalledTimes(1)
+        }
+
     })
 })
